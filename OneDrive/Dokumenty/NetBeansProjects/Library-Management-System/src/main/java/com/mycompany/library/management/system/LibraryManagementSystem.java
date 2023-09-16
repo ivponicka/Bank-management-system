2097ebb7939d11025b34f98be1474870a6db0857
ivponicka <56002673+ivponicka@users.noreply.github.com>
2023-09-16 20:18:10 +0200

    Created the sign-up layout

---
+++ OneDrive/Dokumenty/NetBeansProjects/Library-Management-System/src/main/java/com/mycompany/library/management/system/LibraryManagementSystem.java
@@ -0,0 +1,8 @@
+package com.mycompany.library.management.system;
+
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
